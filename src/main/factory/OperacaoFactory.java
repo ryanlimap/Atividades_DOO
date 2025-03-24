915dd9c1f1d43cd8ebbf6883b8bcd622b8c63715
ryanlimap <pesagaz@hotmail.com>
2025-03-24 20:55:02 -0300

    fix: factory

--- src/main/factory/OperacaoFactory.java
+++ src/main/factory/OperacaoFactory.java
@@ -1,21 +1,19 @@
 package src.main.factory;
 
+import src.main.model.Divisao;
+import src.main.model.Multiplicacao;
 import src.main.model.Operacao;
 import src.main.model.Soma;
 import src.main.model.Subtracao;
-import src.main.model.Multiplicacao;
-import src.main.model.Divisao;
-import java.lang.reflect.Constructor;
 
 public class OperacaoFactory {
-
-    public static Operacao criarOperacao(String tipo) throws Exception {
-        String nomeClasse = tipo.substring(0, 1).toUpperCase() + tipo.substring(1).toLowerCase();
-        String nomeCompleto = "Operacao" + nomeClasse;
-        
-        // Refletir para carregar a classe dinamicamente
-        Class<?> clazz = Class.forName(nomeCompleto);
-        Constructor<?> constructor = clazz.getConstructor();
-        return (Operacao) constructor.newInstance();
+    public static Operacao criarOperacao(String tipo) {
+        return switch (tipo.trim()) {
+            case "+" -> new Soma();
+            case "-" -> new Subtracao();
+            case "*" -> new Multiplicacao();
+            case "/" -> new Divisao();
+            default -> throw new IllegalArgumentException("Operação inválida: " + tipo);
+        };
     }
 }
